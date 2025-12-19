b668655a2d28b7e5b9e057d86faab7306ae5a622
Michaelninder <fabianeternis@gmail.com>
2025-12-19 19:01:06 +0100

    feat: Add APP_title and comment debug-code out

--- src/dice_roll.py
+++ src/dice_roll.py
@@ -1,12 +1,22 @@
 import random
 
+APP_title = """██████╗░██╗░█████╗░███████╗  ██████╗░░█████╗░██╗░░░░░██╗░░░░░
+██╔══██╗██║██╔══██╗██╔════╝  ██╔══██╗██╔══██╗██║░░░░░██║░░░░░
+██║░░██║██║██║░░╚═╝█████╗░░  ██████╔╝██║░░██║██║░░░░░██║░░░░░
+██║░░██║██║██║░░██╗██╔══╝░░  ██╔══██╗██║░░██║██║░░░░░██║░░░░░
+██████╔╝██║╚█████╔╝███████╗  ██║░░██║╚█████╔╝███████╗███████╗
+╚═════╝░╚═╝░╚════╝░╚══════╝  ╚═╝░░╚═╝░╚════╝░╚══════╝╚══════╝\n"""
+
 options = range(1, 7)
 
 def roll(unused):
     return random.choice(options)
 
-for option in options:
-    print(option)
+#for option in options:
+#    print(option)
+
+print(APP_title)
+
 
 while True:
-    print(roll(input("Press enter to roll the dice!")))
+    print(f"Dice rolled to: {roll(input("Press enter to roll the dice!"))}")
