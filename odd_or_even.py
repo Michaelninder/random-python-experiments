53a2deb9843ef812b728d4ef64c615270b2737ac
Michaelninder <fabianeternis@gmail.com>
2025-12-27 22:47:51 +0100

    feat: Finish version 1 of the odd_or_even_generator

---
+++ odd_or_even.py
@@ -0,0 +1,305 @@
+input = input("Enter a number: ") 
+
+if(input == 0):
+    print("The number is Even")
+
+if(input == 1):
+    print("The number is Odd")
+
+if(input == 2):
+    print("The number is Even")
+
+if(input == 3):
+    print("The number is Odd")
+
+if(input == 4):
+    print("The number is Even")
+
+if(input == 5):
+    print("The number is Odd")
+
+if(input == 6):
+    print("The number is Even")
+
+if(input == 7):
+    print("The number is Odd")
+
+if(input == 8):
+    print("The number is Even")
+
+if(input == 9):
+    print("The number is Odd")
+
+if(input == 10):
+    print("The number is Even")
+
+if(input == 11):
+    print("The number is Odd")
+
+if(input == 12):
+    print("The number is Even")
+
+if(input == 13):
+    print("The number is Odd")
+
+if(input == 14):
+    print("The number is Even")
+
+if(input == 15):
+    print("The number is Odd")
+
+if(input == 16):
+    print("The number is Even")
+
+if(input == 17):
+    print("The number is Odd")
+
+if(input == 18):
+    print("The number is Even")
+
+if(input == 19):
+    print("The number is Odd")
+
+if(input == 20):
+    print("The number is Even")
+
+if(input == 21):
+    print("The number is Odd")
+
+if(input == 22):
+    print("The number is Even")
+
+if(input == 23):
+    print("The number is Odd")
+
+if(input == 24):
+    print("The number is Even")
+
+if(input == 25):
+    print("The number is Odd")
+
+if(input == 26):
+    print("The number is Even")
+
+if(input == 27):
+    print("The number is Odd")
+
+if(input == 28):
+    print("The number is Even")
+
+if(input == 29):
+    print("The number is Odd")
+
+if(input == 30):
+    print("The number is Even")
+
+if(input == 31):
+    print("The number is Odd")
+
+if(input == 32):
+    print("The number is Even")
+
+if(input == 33):
+    print("The number is Odd")
+
+if(input == 34):
+    print("The number is Even")
+
+if(input == 35):
+    print("The number is Odd")
+
+if(input == 36):
+    print("The number is Even")
+
+if(input == 37):
+    print("The number is Odd")
+
+if(input == 38):
+    print("The number is Even")
+
+if(input == 39):
+    print("The number is Odd")
+
+if(input == 40):
+    print("The number is Even")
+
+if(input == 41):
+    print("The number is Odd")
+
+if(input == 42):
+    print("The number is Even")
+
+if(input == 43):
+    print("The number is Odd")
+
+if(input == 44):
+    print("The number is Even")
+
+if(input == 45):
+    print("The number is Odd")
+
+if(input == 46):
+    print("The number is Even")
+
+if(input == 47):
+    print("The number is Odd")
+
+if(input == 48):
+    print("The number is Even")
+
+if(input == 49):
+    print("The number is Odd")
+
+if(input == 50):
+    print("The number is Even")
+
+if(input == 51):
+    print("The number is Odd")
+
+if(input == 52):
+    print("The number is Even")
+
+if(input == 53):
+    print("The number is Odd")
+
+if(input == 54):
+    print("The number is Even")
+
+if(input == 55):
+    print("The number is Odd")
+
+if(input == 56):
+    print("The number is Even")
+
+if(input == 57):
+    print("The number is Odd")
+
+if(input == 58):
+    print("The number is Even")
+
+if(input == 59):
+    print("The number is Odd")
+
+if(input == 60):
+    print("The number is Even")
+
+if(input == 61):
+    print("The number is Odd")
+
+if(input == 62):
+    print("The number is Even")
+
+if(input == 63):
+    print("The number is Odd")
+
+if(input == 64):
+    print("The number is Even")
+
+if(input == 65):
+    print("The number is Odd")
+
+if(input == 66):
+    print("The number is Even")
+
+if(input == 67):
+    print("The number is Odd")
+
+if(input == 68):
+    print("The number is Even")
+
+if(input == 69):
+    print("The number is Odd")
+
+if(input == 70):
+    print("The number is Even")
+
+if(input == 71):
+    print("The number is Odd")
+
+if(input == 72):
+    print("The number is Even")
+
+if(input == 73):
+    print("The number is Odd")
+
+if(input == 74):
+    print("The number is Even")
+
+if(input == 75):
+    print("The number is Odd")
+
+if(input == 76):
+    print("The number is Even")
+
+if(input == 77):
+    print("The number is Odd")
+
+if(input == 78):
+    print("The number is Even")
+
+if(input == 79):
+    print("The number is Odd")
+
+if(input == 80):
+    print("The number is Even")
+
+if(input == 81):
+    print("The number is Odd")
+
+if(input == 82):
+    print("The number is Even")
+
+if(input == 83):
+    print("The number is Odd")
+
+if(input == 84):
+    print("The number is Even")
+
+if(input == 85):
+    print("The number is Odd")
+
+if(input == 86):
+    print("The number is Even")
+
+if(input == 87):
+    print("The number is Odd")
+
+if(input == 88):
+    print("The number is Even")
+
+if(input == 89):
+    print("The number is Odd")
+
+if(input == 90):
+    print("The number is Even")
+
+if(input == 91):
+    print("The number is Odd")
+
+if(input == 92):
+    print("The number is Even")
+
+if(input == 93):
+    print("The number is Odd")
+
+if(input == 94):
+    print("The number is Even")
+
+if(input == 95):
+    print("The number is Odd")
+
+if(input == 96):
+    print("The number is Even")
+
+if(input == 97):
+    print("The number is Odd")
+
+if(input == 98):
+    print("The number is Even")
+
+if(input == 99):
+    print("The number is Odd")
+
+if(input == 100):
+    print("The number is Even")
+
--- src/odd_or_even_generator.py
+++ src/odd_or_even_generator.py
@@ -1,6 +1,24 @@
 #default_file_path = "output/"
 #filename = (default_file_path + input("Output filename: ").removesuffix(".py") + ".py")
-filename = input("Output filename: ").removesuffix(".py") + ".py"
+#filename = input("Output filename: ").removesuffix(".py") + ".py"
+filename = "odd_or_even.py"
+max_int = int(input("What should be the end of the \"Odd or Even\" Check? "))
 
+#with open(filename, "w") as f:
+#    print("Hello world", file=f)
+
+
+output = 'input = input("Enter a number: ") \n'
+
+for num in range(max_int + 1):
+    if num % 2 == 0:
+        value = "Even"
+    else:
+        value = "Odd"
+    output += f'''
+if(input == {num}):
+    print("The number is {value}")
+'''
+    
 with open(filename, "w") as f:
-    print("Hello world", file=f)
+    print(output, file=f)
